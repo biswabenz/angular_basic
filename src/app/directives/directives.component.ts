251a8d3eaae48ba3a493f61c4cc3bc6063f19b97
biswabenz <biswajeetkumar407@gmail.com>
2021-05-23 20:09:25 +0530

    testing

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -10,6 +10,16 @@ export class DirectivesComponent implements OnInit {
   isVisible = true;
   dataname = "apple";
   productname = "appl"
+  employeelist =[
+    {name: "Appu", designation: ":MLengineer", age: 25},
+    {name: "jon", designation: ":Manager", age: 45},
+    {name: "ronny", designation: ":web develper", age: 28},
+    {name: "rahul", designation: ":analyst", age: 30},
+    {name: "vikash", designation: ":frontend", age: 28},
+  ]
+   userchoice ="Bike";
+
+
   constructor() { }
 
   ngOnInit(): void {
